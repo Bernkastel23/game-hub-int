d7ce7b9906fcd9f6327d6aa38271a3efb822c4da
Bernkastel <tehokceanz@gmail.com>
2024-11-22 18:32:36 +0100

    refactor - simplyfy time calculations

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,5 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import ms from "ms";
 import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
@@ -6,18 +7,18 @@ import { Platform } from "./usePlatforms";
 const apiClient = new APIClient<Game>('/games');
 
 export interface Game {
-  id: number;
-  name: string;
-  background_image: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-  rating_top: number;
+	id: number;
+	name: string;
+	background_image: string;
+	parent_platforms: { platform: Platform }[];
+	metacritic: number;
+	rating_top: number;
 }
 
 const useGames = (gameQuery: GameQuery) =>
 	useInfiniteQuery<FetchResponse<Game>, Error>({
 		queryKey: ['games', gameQuery],
-		queryFn: ({pageParam = 1}) =>
+		queryFn: ({ pageParam = 1 }) =>
 			apiClient
 				.getAll({
 					params: {
@@ -29,9 +30,9 @@ const useGames = (gameQuery: GameQuery) =>
 					},
 				}),
 		getNextPageParam: (lastPage, allPages) => {
-			return lastPage.next ? allPages.length +1 : undefined;
+			return lastPage.next ? allPages.length + 1 : undefined;
 		},
-		staleTime: 24 *60*60* 1000, // 24h
-});
+		staleTime: ms('24h'),
+	});
 
 export default useGames;
